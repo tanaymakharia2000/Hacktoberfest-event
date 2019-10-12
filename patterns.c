581f60740e23108a3be7e3d13c30a556ec678a52
tanaymakharia2000 <55875294+tanaymakharia2000@users.noreply.github.com>
2019-10-12 17:36:38 +0530

    HELLO WORLDS

---
+++ patterns.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+int n=4;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if (i>=j){
+                printf("*");
+            }
+        }printf("\n");
+    }
+}
+
+
+
+
+
