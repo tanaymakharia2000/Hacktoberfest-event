8358b1cb475b4997ba221a0272b33aa4812839c5
tanaymakharia2000 <55875294+tanaymakharia2000@users.noreply.github.com>
2019-10-13 22:13:55 +0530

    Add files via upload

---
+++ percentage scored.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+printf("write all marks \n");
+float marks_1,marks_2,marks_3,marks_4,marks_5,percentage_scored;
+scanf("%f\n%f\n%f\n%f\n%f",&marks_1,&marks_2,&marks_3,&marks_4,&marks_5);
+percentage_scored = (marks_1+marks_2+marks_3+marks_4+marks_5)/5 ;/* Percentage scored by student*/
+printf("percentage scored = %f %",percentage_scored);
+}
+
+
+
+
